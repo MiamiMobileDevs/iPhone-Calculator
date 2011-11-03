8a0e1bf445fc8cb1274327bcab4094e529572b5c
Nick Rowe <nixterrimus@dcxn.com>
2011-11-03 12:07:28 -0700

    Initial Commit

---
+++ Calculator/CalculatorViewController.h
@@ -0,0 +1,15 @@
+//
+//  CalculatorViewController.h
+//  Calculator
+//
+//  Created by Nicholas Rowe on 11/3/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorViewController : UIViewController {
+    
+}
+
+@end
